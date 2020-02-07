8379e610ad86440768f24c7c7e8c29a1c1e15313
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-07 03:07:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    pig = PigLatinizer.new
+    @piglatin = pig.piglatinize(params[:user_phrase])
+    erb :results
+  end
+  
+  
 end
